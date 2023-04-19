568c2f5ec309b4ab9833375c983d258ea4dac579
Gustavo G Migacz <gustavomigacz@gmail.com>
2023-04-19 16:16:40 -0300

    add Navbar and global styles

---
+++ components/shared/Navbar.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Navbar = (): JSX.Element => {
+  return (
+    <>
+      <div>a</div>
+    </>
+  );
+};
+
+export default Navbar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
 import type { AppProps } from "next/app";
+import GlobalStyle from "@/styles/global";
+import Navbar from "@/components/shared/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+      <Navbar />
+    </>
+  );
 }
