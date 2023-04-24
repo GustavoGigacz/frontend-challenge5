45de8282363e096c7170cdf4ffb4b5655e7ee9f7
Gustavo G Migacz <gustavomigacz@gmail.com>
2023-04-24 15:46:49 -0300

    add Image and svg

---
+++ components/icons/Wave.tsx
@@ -0,0 +1,20 @@
+const Wave = (): JSX.Element => {
+  return (
+    <svg
+      width='1440'
+      height='73'
+      viewBox='0 0 1440 73'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M0 72.7278C0 72.7278 478.202 0 720 0C961.798 0 1440 72.7278 1440 72.7278H0Z'
+        fill='#F8F8FE'
+      />
+    </svg>
+  );
+};
+
+export default Wave;
