d7c0382e485ac2dee2e5b37152d097db2a914ada
Gustavo G Migacz <gustavomigacz@gmail.com>
2023-04-25 14:54:38 -0300

    add image and svg

--- components/icons/Wave.tsx
+++ components/icons/Wave.tsx
@@ -1,8 +1,7 @@
 const Wave = (): JSX.Element => {
   return (
     <svg
-      width='1440'
-      height='73'
+      width='100%'
       viewBox='0 0 1440 73'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
